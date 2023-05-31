e82f85cf791722c7b825bb1cdfdfb4b7346b9d5f
cold_hailstone <cksn1993@gmail.com>
2023-05-31 14:25:06 +0900

    기본 객체들

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,6 +1,8 @@
 package hello.thymeleafbasic.basic;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,8 +49,23 @@ public class BasicController {
         return "/basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
     @Data
     static class User {
+
         private String username;
         private int age;
 
